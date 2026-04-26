e5b7044ae9f029e0241b1d7984d8eb6e86bb258a
Howardm915 <67358595+Howardm915@users.noreply.github.com>
2026-04-26 00:52:28 -0400

    Change default response message in helloHandler

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello World!"
+		response = "Everything is working!"
 	}
 
 	fmt.Fprintln(w, response)
